ba4a0f7030411ec42230b0255e177eb9fc4b73eb
Andreas Löf <andreas@alternating.net>
2015-03-02 00:19:59 +1300

    Added logging to file

--- app/src/main/java/net/alternating/movementsender/MainActivity.java
+++ app/src/main/java/net/alternating/movementsender/MainActivity.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -30,11 +35,26 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     private DatagramSocket socket;
     private String host = "goddess";
     private int port = 22222;
-    private Button sendButton;
+    private Button logButton;
     private SensorManager sensorManager;
     private Sensor rotationSensor;
     private Sensor accelSensor;
-    private final String TAG = "net.alternating.";
+    private final String TAG = "MainActivity";
+    private boolean loggingEnabled = false;
+    private PrintStream rotationStream;
+    private PrintStream accelerationStream;
+
+    private static final String rotationFilename = "rotationFile.txt";
+    private static final String accelerationFilename = "accelerationFile.txt";
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +63,64 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
         hostView = (TextView) findViewById(R.id.host);
         portView = (TextView) findViewById(R.id.port);
-        sendButton = (Button) findViewById(R.id.send_button);
+        logButton = (Button) findViewById(R.id.log_button);
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("BUTTON", "Trying to send data");
-                new SendRotationTask().execute(0f, 0f, 0f, 0f, 0f);
+                if (!loggingEnabled) {
+                    Log.d(TAG, "Starting Logging");
+                    loggingEnabled = true;
+                    logButton.setText("Stop Logging");
+                    try {
+                        if (isExternalStorageWritable()) {
+                            Log.d(TAG,"External storage for logging");
+                            File rotationFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), rotationFilename);
+                            File accelerationFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), accelerationFilename);
+                            if (rotationFile.exists())
+                                rotationFile.delete();
+                            rotationFile.createNewFile();
+                            if (accelerationFile.exists())
+                                accelerationFile.delete();
+                            accelerationFile.createNewFile();
+                            Log.d(TAG,rotationFile.getAbsolutePath());
+                            rotationStream = new PrintStream(new FileOutputStream(rotationFile));
+                            accelerationStream = new PrintStream(new FileOutputStream(accelerationFile));
+
+                            rotationFile.setReadable(true,false);
+                            accelerationFile.setReadable(true,false);
+                        }
+                        else {
+                            Log.d(TAG, "Internal storage for logging");
+                            rotationStream = new PrintStream(openFileOutput(rotationFilename, Context.MODE_WORLD_READABLE));
+                            accelerationStream = new PrintStream(openFileOutput(accelerationFilename, Context.MODE_WORLD_READABLE));
+                        }
+                    } catch (Exception e) {
+                        loggingEnabled = false;
+                        Log.e(TAG, "Failed to start Logging!!");
+                        Log.e(TAG,e.getMessage());
+                        e.printStackTrace();
+                        logButton.setText("Logging broke");
+                        logButton.setClickable(false);
+                    }
+                }
+                else {
+                    try {
+                        Log.d(TAG, "Stopping Logging");
+                        rotationStream.flush();
+                        rotationStream.close();
+                        rotationStream = null;
+                        accelerationStream.flush();
+                        accelerationStream.close();
+                        accelerationStream = null;
+                        loggingEnabled = false;
+                        logButton.setText("Start Logging");
+                    } catch (Exception e) {
+                        Log.e(TAG, "Error while stopping logging!!");
+                        Log.e(TAG,e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
             }
         });
 
@@ -90,19 +161,31 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         return super.onOptionsItemSelected(item);
     }
 
-    private float[] fs = new float[4];
+
     private long old_ts = -1;
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         //Log.d("SENSOR", Arrays.toString(event.values));
-
+        float[] fs = new float[4];
         if (event.sensor == rotationSensor) {
             SensorManager.getQuaternionFromVector(fs, event.values);
             new SendRotationTask().execute(fs[0],
                     fs[1],
                     fs[2],
                     fs[3]);
+            if (loggingEnabled) {
+                try {
+                    rotationStream.println(System.currentTimeMillis() + "," + fs[0] + "," + fs[1] + "," + fs[2] + "," + fs[3]);
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to write rotation log data!!");
+                    Log.e(TAG, e.getMessage());
+                    e.printStackTrace();
+                    loggingEnabled = false;
+                    logButton.setText("Logging broke");
+                    logButton.setClickable(false);
+                }
+            }
         }
         else {
             if (old_ts == -1)
@@ -110,13 +193,28 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
             long diff = System.currentTimeMillis() - old_ts;
             old_ts = System.currentTimeMillis();
             double diff_sec = diff / 1000d;
-            Log.w("agi", "diff: "+ diff + " sdiff:" + diff_sec);
+
             new SendAccelTask().execute(
                     new Float(diff_sec),
                     event.values[0],
                     event.values[1],
                     event.values[2]
             );
+            if (loggingEnabled) {
+                try {
+                    accelerationStream.println(System.currentTimeMillis() + "," +
+                            event.values[0] + "," +
+                            event.values[1] + "," +
+                            event.values[2]);
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to write acceleration log data!!");
+                    Log.e(TAG, e.getMessage());
+                    e.printStackTrace();
+                    loggingEnabled = false;
+                    logButton.setText("Logging broke");
+                    logButton.setClickable(false);
+                }
+            }
         }
     }
 
